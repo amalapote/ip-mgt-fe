cc6b2c46bcf487df90d2e3cd127300de8dab6818
Apollo Malapote <apollo.malapote@cabrera.ca>
2024-06-24 04:59:45 +0800

    Removing unused button

--- src/views/Home/EditView.vue
+++ src/views/Home/EditView.vue
@@ -52,7 +52,6 @@
             <span v-if="getLoadingAddIp"> Loading....</span>
             <span v-else> Update IP {{ details.ip_address }}</span>
         </button>
-        <button @click="toggleVisibility = false" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"><span v-if="getLoading"> Loading Page</span> <span v-else> Refresh </span></button>
     </form>
     </div>
 </template>
